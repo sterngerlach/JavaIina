626f08fad44d59f2c9c32542e8cc853a41cb74e0
Stern Gerlach <drksugi@gmail.com>
2018-06-23 15:49:06 +0900

    Create event listener skeletons in RegisterMemberViewController class

--- src/javaiina/RegisterMemberViewController.java
+++ src/javaiina/RegisterMemberViewController.java
@@ -3,6 +3,8 @@
 
 package javaiina;
 
+import java.awt.event.ItemEvent;
+
 public class RegisterMemberViewController
 {
     private RegisterMemberView mView;
@@ -14,5 +16,22 @@ public class RegisterMemberViewController
     {
         this.mView = registerMemberView;
         this.mModel = registerMemberViewModel;
+        
+        this.mView.addPrefectureChangeListener(e -> {
+            if (e.getStateChange() != ItemEvent.SELECTED)
+                return;
+            
+            // You can update the prefecture property of the model (if needed)
+            // Retrieve the prefecture name by calling this.mView.getSelectedPrefectureName()
+            // Then call Prefecture.getPrefectureIdFromName() to get the prefecture Id from the prefecture name
+        });
+        
+        this.mView.addRegisterButtonClickListener(e -> {
+            // TODO: It may also be possible to create an inner class to handle the event
+        });
+        
+        this.mView.addCancelButtonClickListener(e -> {
+            // TODO: It may also be possible to create an inner class to handle the event
+        });
     }
 }
