b1850258ccc4914a42fd85b897244144e8d6c79f
Stern Gerlach <drksugi@gmail.com>
2018-06-23 15:49:47 +0900

    Simplify RegisterMemberViewModel class

--- src/javaiina/RegisterMemberViewModel.java
+++ src/javaiina/RegisterMemberViewModel.java
@@ -3,29 +3,14 @@
 
 package javaiina;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class RegisterMemberViewModel
 {
-    private String mFirstName;
-    private String mSecondName;
-    private String mFirstNameKana;
-    private String mSecondNameKana;
-    private String mNickName;
-    private ZonedDateTime mBirthDate;
-    private Gender mGender;
-    private String mPhoneNumber;
-    private String mEmailAddress;
+    public static final LocalDate MinBirthDate = LocalDate.of(1900, Month.JANUARY, 1);
     
-    public String firstName() { return this.mFirstName; }
-    public String secondName() { return this.mSecondName; }
-    public String firstNameKana() { return this.mFirstNameKana; }
-    public String secondNameKana() { return this.mSecondNameKana; }
-    public String nickName() { return this.mNickName; }
-    public ZonedDateTime birthDate() { return this.mBirthDate; }
-    public Gender gender() { return this.mGender; }
-    public String phoneNumber() { return this.mPhoneNumber; }
-    public String emailAddress() { return this.mEmailAddress; }
+    private Member mMember;
     
     public RegisterMemberViewModel()
     {
@@ -34,14 +19,6 @@ public class RegisterMemberViewModel
     
     public void reset()
     {
-        this.mFirstName = null;
-        this.mSecondName = null;
-        this.mFirstNameKana = null;
-        this.mSecondNameKana = null;
-        this.mNickName = null;
-        this.mBirthDate = null;
-        this.mGender = null;
-        this.mPhoneNumber = null;
-        this.mEmailAddress = null;
+        this.mMember = null;
     }
 }
