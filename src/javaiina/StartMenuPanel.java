7e98d4fb11d2815138ec2b97adcae5782ce71005
Stern Gerlach <drksugi@gmail.com>
2018-07-11 14:07:35 +0900

    StartMenuPanel class derives from PanelBase class

--- src/javaiina/StartMenuPanel.java
+++ src/javaiina/StartMenuPanel.java
@@ -3,74 +3,45 @@
 
 package javaiina;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 
-public class StartMenuPanel extends JPanel
+public class StartMenuPanel extends PanelBase
 {
     private static final long serialVersionUID = 2107679826433799133L;
     
-    private JPanel mPanelHeader;
-    private JPanel mPanelCenter;
-    
-    private Font mFontHeaderTitle;
-    private JLabel mLabelHeaderTitle;
-    private Font mFontHeaderDescription;
-    private JLabel mLabelHeaderDescription;
+    private MainModel mMainModel;
     
     private JButton mButtonLogin;
     private JButton mButtonRegisterMember;
     
-    public StartMenuPanel()
+    @Override
+    public String getPanelName()
     {
-        this.initializeComponent();
+        return "Start";
     }
     
-    private void initializeComponent()
+    public StartMenuPanel()
     {
-        /* Border */
-        Border defaultBorder = new EmptyBorder(10, 10, 10, 10);
-        
-        /* Container */
-        this.setLayout(new BorderLayout());
-        
-        this.mPanelHeader = new JPanel();
-        this.mPanelHeader.setLayout(new BoxLayout(this.mPanelHeader, BoxLayout.Y_AXIS));
-        this.mPanelHeader.setBorder(defaultBorder);
-        this.add(this.mPanelHeader, BorderLayout.NORTH);
-        
-        this.mPanelCenter = new JPanel();
-        this.mPanelCenter.setLayout(new BoxLayout(this.mPanelCenter, BoxLayout.Y_AXIS));
-        this.mPanelCenter.setBorder(defaultBorder);
-        this.add(this.mPanelCenter, BorderLayout.CENTER);
+    }
+    
+    public void setModel(MainModel mainModel)
+    {
+        this.mMainModel = mainModel;
+    }
+    
+    @Override
+    protected void initializeComponent()
+    {
+        super.initializeComponent();
         
         /* Header */
-        this.mFontHeaderTitle = new Font(Font.DIALOG, Font.BOLD, 18);
-        this.mLabelHeaderTitle = new JLabel("Welcome");
-        this.mLabelHeaderTitle.setFont(this.mFontHeaderTitle);
-        this.mPanelHeader.add(this.mLabelHeaderTitle);
-        
-        this.mPanelHeader.add(Box.createVerticalStrut(10));
-        
-        this.mFontHeaderDescription = new Font(Font.DIALOG, Font.PLAIN, 16);
-        this.mLabelHeaderDescription = new JLabel(
-            "Welcome to Javaiina - Costume rental management system.");
-        this.mLabelHeaderDescription.setFont(this.mFontHeaderDescription);
-        this.mPanelHeader.add(this.mLabelHeaderDescription);
-        
-        this.mPanelHeader.add(Box.createVerticalStrut(10));
-        this.mPanelHeader.add(new JSeparator(JSeparator.HORIZONTAL));
+        this.setHeaderTitle("Welcome");
+        this.setHeaderDescription("Welcome to Javaiina - Costume rental management system.");
         
         /* Center */
         Font fontButton = new Font(Font.DIALOG, Font.PLAIN, 16);
