c13c36fd33bcbaaab5a1edba69dabb34ccdc5318
Stern Gerlach <drksugi@gmail.com>
2018-07-11 14:08:50 +0900

    MainMenuPanel class derives from PanelBase class

--- src/javaiina/MainMenuPanel.java
+++ src/javaiina/MainMenuPanel.java
@@ -3,86 +3,63 @@
 
 package javaiina;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 
-public class MainMenuPanel extends JPanel
+public class MainMenuPanel extends PanelBase
 {
     private static final long serialVersionUID = 520562663600631546L;
     
-    private JPanel mPanelHeader;
-    private JPanel mPanelCenter;
+    private MainModel mMainModel;
     
-    private Font mFontHeaderTitle;
-    private JLabel mLabelHeaderTitle;
-    private Font mFontHeaderDescription;
-    private JLabel mLabelHeaderDescription;
-    
-    private JButton mButtonSearchAndBorrow;
+    private JButton mButtonShowItems;
     private JButton mButtonShowBorrowingItems;
     private JButton mButtonShowMemberInfo;
     private JButton mButtonLogout;
     
-    public MainMenuPanel()
+    @Override
+    public String getPanelName()
     {
-        this.initializeComponent();
+        return "Main Menu";
     }
     
-    private void initializeComponent()
+    public MainMenuPanel()
     {
-        /* Border */
-        Border defaultBorder = new EmptyBorder(10, 10, 10, 10);
+    }
+    
+    public void setModel(MainModel mainModel)
+    {
+        this.mMainModel = mainModel;
         
-        /* Container */
-        this.setLayout(new BorderLayout());
-        
-        this.mPanelHeader = new JPanel();
-        this.mPanelHeader.setLayout(new BoxLayout(this.mPanelHeader, BoxLayout.Y_AXIS));
-        this.mPanelHeader.setBorder(defaultBorder);
-        this.add(this.mPanelHeader, BorderLayout.NORTH);
-        
-        this.mPanelCenter = new JPanel();
-        this.mPanelCenter.setLayout(new BoxLayout(this.mPanelCenter, BoxLayout.Y_AXIS));
-        this.mPanelCenter.setBorder(defaultBorder);
-        this.add(this.mPanelCenter, BorderLayout.CENTER);
+        if (this.mMainModel.loggedInMember() == null)
+            this.setUserName("(Anonymous)");
+        else
+            this.setUserName(this.mMainModel.loggedInMember().nickName());
+    }
+    
+    @Override
+    protected void initializeComponent()
+    {
+        super.initializeComponent();
         
         /* Header */
-        this.mFontHeaderTitle = new Font(Font.DIALOG, Font.BOLD, 18);
-        this.mLabelHeaderTitle = new JLabel("Main Menu");
-        this.mLabelHeaderTitle.setFont(this.mFontHeaderTitle);
-        this.mPanelHeader.add(this.mLabelHeaderTitle);
-        
-        this.mPanelHeader.add(Box.createVerticalStrut(10));
-        
-        this.mFontHeaderDescription = new Font(Font.DIALOG, Font.PLAIN, 16);
-        this.mLabelHeaderDescription = new JLabel("Welcome back!");
-        this.mLabelHeaderDescription.setFont(this.mFontHeaderDescription);
-        this.mPanelHeader.add(this.mLabelHeaderDescription);
-        
-        this.mPanelHeader.add(Box.createVerticalStrut(10));
-        this.mPanelHeader.add(new JSeparator(JSeparator.HORIZONTAL));
+        this.setHeaderTitle("Main Menu");
+        this.setHeaderDescription("Welcome back!");
         
         /* Center */
         Font fontButton = new Font(Font.DIALOG, Font.PLAIN, 16);
         Dimension buttonSize = new Dimension(300, 30);
         
-        /* Search And Borrow Button */
-        this.mButtonSearchAndBorrow = new JButton("Search & Borrow");
-        this.mButtonSearchAndBorrow.setFont(fontButton);
-        this.mButtonSearchAndBorrow.setPreferredSize(buttonSize);
-        this.mButtonSearchAndBorrow.setMaximumSize(buttonSize);
-        this.mPanelCenter.add(this.mButtonSearchAndBorrow);
+        /* Show Items Button */
+        this.mButtonShowItems = new JButton("Show Items");
+        this.mButtonShowItems.setFont(fontButton);
+        this.mButtonShowItems.setPreferredSize(buttonSize);
+        this.mButtonShowItems.setMaximumSize(buttonSize);
+        this.mPanelCenter.add(this.mButtonShowItems);
         
         this.mPanelCenter.add(Box.createHorizontalGlue());
         this.mPanelCenter.add(Box.createVerticalStrut(10));
@@ -118,9 +95,14 @@ public class MainMenuPanel extends JPanel
         this.mPanelCenter.add(Box.createVerticalGlue());
     }
     
-    public void addSearchAndBorrowListener(ActionListener actionListener)
+    private void setUserName(String userName)
     {
-        this.mButtonSearchAndBorrow.addActionListener(actionListener);
+        this.setHeaderDescription("Welcome back, " + userName + "!");
+    }
+    
+    public void addShowItemsListener(ActionListener actionListener)
+    {
+        this.mButtonShowItems.addActionListener(actionListener);
     }
     
     public void addShowBorrowingItemsListener(ActionListener actionListener)
@@ -136,10 +118,5 @@ public class MainMenuPanel extends JPanel
     public void addLogoutListener(ActionListener actionListener)
     {
         this.mButtonLogout.addActionListener(actionListener);
-    }
-    
-    public void setUserName(String userName)
-    {
-        this.mLabelHeaderDescription.setText("Welcome back, " + userName + "!");
     }
 }
