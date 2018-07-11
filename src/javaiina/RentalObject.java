d3a754a095bce923948622cb1c5704fb8334609a
Stern Gerlach <drksugi@gmail.com>
2018-07-11 14:13:41 +0900

    RentalObject can have multiple RentalObjectSizeInfo

--- src/javaiina/RentalObject.java
+++ src/javaiina/RentalObject.java
@@ -8,23 +8,23 @@ public class RentalObject
     private int mId;
     private String mName;
     private String mCategoryName;
-    private RentalObjectSizeInfo mSizeInfo;
+    private RentalObjectSizeInfo[] mAvailableSizeInfo;
     private int mCost;
     
     public int id() { return this.mId; }
     public String name() { return this.mName; }
     public String categoryName() { return this.mCategoryName; }
-    public RentalObjectSizeInfo sizeInfo() { return this.mSizeInfo; }
+    public RentalObjectSizeInfo[] availableSizeInfo() { return this.mAvailableSizeInfo; }
     public int cost() { return this.mCost; }
     
     public RentalObject(
         int id, String name, String categoryName,
-        RentalObjectSizeInfo sizeInfo, int cost)
+        RentalObjectSizeInfo[] availableSizeInfo, int cost)
     {
         this.mId = id;
         this.mName = name;
         this.mCategoryName = categoryName;
-        this.mSizeInfo = sizeInfo;
+        this.mAvailableSizeInfo = availableSizeInfo;
         this.mCost = cost;
     }
 }
