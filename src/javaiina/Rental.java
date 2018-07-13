e87f3fc1d9a18be3cd6147147503cc7e849712de
Stern Gerlach <drksugi@gmail.com>
2018-07-13 20:27:41 +0900

    Add a new member in Rental class

--- src/javaiina/Rental.java
+++ src/javaiina/Rental.java
@@ -4,25 +4,26 @@
 package javaiina;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
-public class Rental {
+public class Rental
+{
     private Member mMember;
     private RentalObject mRentalObject;
+    private RentalObjectSizeInfo mSizeInfo;
     private LocalDate mBeginDate;
     private LocalDate mDesiredReturnDate;
     private LocalDate mActualReturnDate;
     private int mOverduePayment;
     
-    public Rental(Member member, RentalObject rentalObject, 
-                  LocalDate beginDate, LocalDate desiredReturnDate, LocalDate actualReturnDate,
-                  int overduePayment)
+    public Rental(
+        Member member, RentalObject rentalObject,
+        RentalObjectSizeInfo sizeInfo, 
+        LocalDate beginDate, LocalDate desiredReturnDate,
+        LocalDate actualReturnDate, int overduePayment)
     {
         this.mMember = member;
         this.mRentalObject = rentalObject;
+        this.mSizeInfo = sizeInfo;
         this.mBeginDate = beginDate;
         this.mDesiredReturnDate = desiredReturnDate;
         this.mActualReturnDate = actualReturnDate;
@@ -30,9 +31,20 @@ public class Rental {
     }
     
     public Member getMember() { return this.mMember; }
-    public RentalObject getRentalObject() { return this.mRentalObject;}
+    public RentalObject getRentalObject() { return this.mRentalObject; }
+    public RentalObjectSizeInfo getSizeInfo() { return this.mSizeInfo; }
     public LocalDate getBeginDate() { return this.mBeginDate; }
     public LocalDate getDesiredReturnDate() { return this.mDesiredReturnDate; }
     public LocalDate getActualReturnDate() { return this.mActualReturnDate; }
     public int getOverduePayment() { return this.mOverduePayment; }
+    
+    public void setActualReturnDate(LocalDate actualReturnDate)
+    {
+        this.mActualReturnDate = actualReturnDate;
+    }
+    
+    public void setOverduePayment(int overduePayment)
+    {
+        this.mOverduePayment = overduePayment;
+    }
 }
