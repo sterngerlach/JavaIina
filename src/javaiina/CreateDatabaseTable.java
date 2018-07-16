90fdd1eb0c12d41261dac4811543e95598d42aed
taikotea777 <m.t.yagura_piano@icloud.com>
2018-07-16 23:02:03 +0900

    modify

--- src/javaiina/CreateDatabaseTable.java
+++ src/javaiina/CreateDatabaseTable.java
@@ -13,33 +13,39 @@ public class CreateDatabaseTable extends DatabaseAccess{
     
     private void createMemberTable() throws SQLException{
         String memberTable = 
-            "create table Member (" +
-            "memberId bigint primary key," +
-            "firstName varchar(128)," +
-            "secondName varchar(128)," +
-            "nickName varchar(128)," +
-            "birthData datetime," +
-            "registerData datetime," +
-            "gender varchar(64)," +
-            "phoneNumber varchar(128)," +
-            "emailAddress varchar(128)," +
-            ")";
+            "create table Member ("
+            + "memberId bigint primary key,"
+            + "firstName varchar(128),"
+            + "secondName varchar(128),"
+            + "firstName varchar(256),"
+            + "secondName varchar(256),"
+            + "nickName varchar(128),"
+            + "birthDate date,"
+            + "registerDate date,"
+            + "gender varchar(64),"
+            + "address varchar(512),"
+            + "postCode varchar(128)"
+            + "phoneNumber varchar(128)," 
+            + "emailAddress varchar(128),"
+            + ")";
         
         mStmt = mConnToDatabase.createStatement();
         mStmt.execute(memberTable);
         mStmt.close();
     }
 
-    private void createRentalObjectTable() throws SQLException{
+    private void createRentalTable() throws SQLException{
         String rentalObjectTable = 
-            "create table RentalObject (" +
-            "memberId bigint primary key," +
-            "rentalObject varchar(256)," +
-            "beginDate date," +
-            "desiredReturnDate date," +
-            "actualReturnDate date," +
-            "overduePayment int"+
-            ")";
+            "create table RentalObject ("
+            + "rentalId bigint,"
+            + "memberId bigint,"
+            + "rentalObject varchar(256),"
+            + "sizeId int,"
+            + "beginDate date,"
+            + "desiredReturnDate date,"
+            + "actualReturnDate date," 
+            + "overduePayment int"
+            + ")";
         
         mStmt = mConnToDatabase.createStatement();
         mStmt.execute(rentalObjectTable);
@@ -48,62 +54,63 @@ public class CreateDatabaseTable extends DatabaseAccess{
 
     private void createReservationTable() throws SQLException{
         String reservationTable =
-            "create table Reservation (" +
-            "memberId bigint primary key," +
-            "rentalObject varchar(256)," +
-            "reservationData varchar(256)" +
-            ")";
+            "create table Reservation ("
+            + "reservationId bigint"
+            + "memberId bigint,"
+            + "rentalId bigint,"
+            + "reservationDate date,"
+            + "sizeId int,"
+            + "done bit,"
+            + ")";
         
         mStmt = mConnToDatabase.createStatement();
         mStmt.execute(reservationTable);
         mStmt.close();	
     }
     
-    private void createAvailableSizeInfoTable() throws SQLException{
-        String availableSizeInfoTable =
-            "create talbe AvailableSizeInfo (" +
-            "rentalObjectId bigint," +
-            "sizeId int," +
-            /*"rentalObject," +
-            "height int," +
-            "weight int," +
-            "waistMin int," +
-            "waistMax int" +*/
-            ")";
+    private void createRentalObjectTable() throws SQLException{
+        String rentalTable =
+            "create table RentalObject ("
+            + "rentalObjectId int,"
+            + "rentalObjectName varchar(256),"
+            + "categoryName varchar(256),"
+            + "cost int"
+            + ")";
         
         mStmt = mConnToDatabase.createStatement();
-        mStmt.execute(availableSizeInfoTable);
+        mStmt.execute(rentalTable);
         mStmt.close();
     }
 
     private void createSizeInfoTable() throws SQLException{
         String sizeInfoTable =
-            "create table SizeInfo (" +
-            "sizeId int primary key,"+
-            "sizeName varchar(64)," +
-            "weight int," +
-            "waistMin int," +
-            "waistMax int," +
-            "chestMeasure int," +
-            "shoulderWidth int,"+
-            "lenSleeve int," +
-            "inseam int" +
-            ")";
+            "create table SizeInfo ("
+            + "sizeId int primary key,"
+            + "sizeName varchar(64),"
+            + "height int,"
+            + "weight int,"
+            + "waistMin int,"
+            + "waistMax int,"
+            + "chestMeasure int,"
+            + "shoulderWidth int,"
+            + "lenSleeve int,"
+            + "inseam int"
+            + ")";
         
         mStmt = mConnToDatabase.createStatement();    
         mStmt.executeQuery(sizeInfoTable);
         mStmt.close();
     }
     
-    /*private void createGenderTable() throws SQLException{
-        String genderTable =
-            "create table Gender(" +
-            "genderId int," +
-            "gender vatchar(64)" +
-            ")";
+    private void createAvailableSizeInfoTable() throws SQLException{
+        String availableSizeInfoTable =
+            "create table AvailableSizeInfo("
+            + "rentalObjectId bigint,"
+            + "sizeId int"
+            +")";
         
         mStmt = mConnToDatabase.createStatement();
-        mStmt.execute(genderTable);
+        mStmt.execute(availableSizeInfoTable);
         mStmt.close();
-    }*/
+    }
 }
