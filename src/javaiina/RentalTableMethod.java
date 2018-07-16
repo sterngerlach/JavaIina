e61d442dc765e12abd29abc0a91b7ec5c4f84a27
taikotea777 <m.t.yagura_piano@icloud.com>
2018-07-16 22:25:02 +0900

    rental table method

--- src/javaiina/RentalTableMethod.java
+++ src/javaiina/RentalTableMethod.java
@@ -1,5 +1,19 @@
 package javaiina;
 
 public class RentalTableMethod {
-
+    public RentalTableMethod() throws SQLException, ClassNotFoundException{
+        super();
+    }
+    
+    public void rentalInsert(Rental ren) throws SQLException{
+        mStmt = mConnToDatabase.createStatement();
+        mStmt.executeUpdate("insert into Rental values("
+            + ren.rentalId() + ","
+            + ren.getMember().id() + ","
+            + ren.getRentalObject().id() + ","
+            + ren.reservationDate() + ","
+            + ")"
+        );
+        mStmt.close();
+    }
 }
