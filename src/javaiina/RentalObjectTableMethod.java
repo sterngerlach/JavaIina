4ef80b291f30b3d3a9a691409cf5a13e3f98e523
ua210681 <ua210681@y14d008.educ.cc.keio.ac.jp>
2018-07-17 18:58:46 +0900

    add RentalObjectTableMethod

--- src/javaiina/RentalObjectTableMethod.java
+++ src/javaiina/RentalObjectTableMethod.java
@@ -1,5 +1,100 @@
 package javaiina;
 
 public class RentalObjectTableMethod {
+    public RentalObjectTableMethod() throws SQLException, ClassNotFoundException{
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        if(tableExist("RentalObject"))
+            stmt.executeUpdate("insert into RentalObject values" 
+                + "(0, 'スーツレンタル', 'スーツ', 2000)"
+                + ",(1, '制服レンタル', 'スカート', 4000)"
+                + ",(2, 'コスプレレンタル', 'コスプレ衣装', 10000)"
+              );
+        stmt.close();
+    }
+    
+    public void rentalObjectInsert(RentalObject ro) throws SQLException{
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = mConnToDatabase.createStatement();
+        stmt.executeUpdate("insert into RentalObject values("
+            + ro.id() + ","
+            + "'" + ro.name() + "',"
+            + "'" + ro.categoryName() + "',"
+            + ro.cost() + ","
+              + ")"
+         );
+        stmt.close();
+    }
 
+    public void rentalObjectDelete(RentalObject ro) throws SQLException{
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("delete from RentalObject where rentalObjectId = " + ro.id());
+        stmt.close();
+    }
+    public void rentalObjectUpdate(String column, String updateTada, String condition) throws SQLException{
+        Connection conn = Database.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject "
+            + "set " + column + " = " + "'" + updateData + "' "
+            + condition
+         );
+        stmt.close();
+    }
+    
+    public void rentalObjectUpdate(String column, int updateData, String condition) throws SQLException {
+        Connection conn = Database.getInstance().getConnection();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject "
+            + "set " + column + " = " + updateData + " "
+            + condition
+         );
+        stmt.close();
+    }
+    
+    public void rentalObjectUpdate(String column, long updateData, String condition) throws SQLException{
+        Connection conn = Database.getInstance().getConnection();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject "
+            +"set " + column + " = " + updateData + " " 
+            + condition
+         );
+        stmt.close();
+    }
+    
+    public void rentalObjectUpdateRentalObjectName(RentalObject ro) throws SQLException{
+        Connection conn = Database.getInstance().getConnetion();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject "
+            + "set rentalObjectName = '" + ro.name() + "' " 
+            + "where rentalObjectId = " + ro.id()
+         ); 
+        stmt.close();
+    }
+    
+    public void rentalObjectUpdateCategoryName(RentalObject ro) throws SQLException{
+        Connection conn = Database.getInstance().getConnection();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject "
+            + "set categoryName = " + ro.categoryName() + " "  
+            + "where rentalObjectId = " + ro.id()
+         ); 
+        stmt.close();
+    }
+    
+    public void rentalObjectUpdateCost(RentalObject ro) throws SQLException{
+        Connection conn = Database.getInstance().getConnetion();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(
+            "update RentalObject"
+            +" set cost = " + ro.cost() + " "
+            + "where rentalObjectId = " + ro.id()
+         ); 
+        stmt.close();
+    }
 }
