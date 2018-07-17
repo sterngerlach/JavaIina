67e47f803db3c04662932981b2943f8784a8a852
松本　武大 <@DESKTOP-FNM7UHH>
2018-07-17 16:01:11 +0900

    create MemberTableMethod

--- src/javaiina/MemberTableMethod.java
+++ src/javaiina/MemberTableMethod.java
@@ -3,17 +3,19 @@
 
 package javaiina;
 
+import java.sql.*;
 import java.text.SimpleDateFormat;
 
-public class MemberTableMethod extends DatabaseAccess{
+public class MemberTableMethod{
     
     public MemberTableMethod() throws SQLException, ClassNotFoundException{
         super();
     }
     
-    public void memberInsert(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate("insert into Member values("
+    public void memberInsert(Member m) throws SQLException, ClassNotFoundException{
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("insert into Member values("
             + m.id() + ","
             + "'" + m.firstName() + "',"
             + "'" + m.secondName() + "',"
@@ -22,126 +24,138 @@ public class MemberTableMethod extends DatabaseAccess{
             + "'" + m.nickName() + "',"
             + m.birthDate() + ","
             + m.registerDate() + ","
-            + m.gender() + ","
+            + "'" + m.gender().toString() + "',"
             + "'" + m.phoneNumber() + "',"
             + "'" + m.emailAddress() + "'," +
             ")"
         );
-        mStmt.close();
+        stmt.close();
     }
 
     public void memberDelete(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "delete from Member where memberId = " + m.id());
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdate(String column, String updateData, String condition) {
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member "
              + "set " + column + " = " + updateData + " "
              + condition
         );
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdate(String column, Date updateData, String condition) {
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member "
             + "set " + column + " = " + updateData + " "
             + condition
         );
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdate(String column, long updateData, String condition) {
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member " 
             + "set " + column + " = " + updateData + " " 
             + condition
         );
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateFirstName(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member "
             + "set firstName = " + "'" + m.firstName() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateSecondName(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member "
-            + "set secondName = " + "'" + m.secontName() + "' " 
+            + "set secondName = " + "'" + m.secondName() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateFirstNameKana(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member"
             +" set firstNameKana = " + "'" + m.firstNameKana() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateSecondNameKana(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member"
             +" set secondNameKana = " + "'" + m.secondNameKana() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateNickName(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member"
             + "set nickName = " + "'" + m.nickName() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdatePhoneNumber(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member"
             +" set phoneNumber = " + "'" + m.phoneNumber() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
     
     public void memberUpdateEmailAddress(Member m) throws SQLException{
-        mStmt = mConnToDatabase.createStatement();
-        mStmt.executeUpdate(
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
             "update Member"
             +" set emailAddress = " + "'" + m.emailAddress() + "' " 
             + "where memberId = " + m.id()
         ); 
-        mStmt.close();
+        stmt.close();
     }
 
     public ArrayList<ArrayList<String>> memberSelectAll(Member m) throws SQLException{
         ArrayList<ArrayList<String>> resultListList = new ArrayList<ArrayList<String>>();
         ArrayList<String> resultList = new ArrayList<String>();
-        mStmt = mConnToDatabase.createStatement();
-        ResultSet rs = mStmt.executeQuery("select * from MemberDataTest where mId = " + m.id());
+        Connection conn = DatabaseAccess.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from MemberDataTest where mId = " + m.id());
         while(rs.next()) {
             resultList.add(String.valueOf(rs.getInt("memberId")));
             resultList.add(rs.getString("firstName"));
@@ -158,7 +172,7 @@ public class MemberTableMethod extends DatabaseAccess{
             removeAll(resultList);
         }
         rs.close();
-        mStmt.close();
+        stmt.close();
         return resultListList;
     }
     
