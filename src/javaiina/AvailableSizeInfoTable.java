7c64bbefa3411aa59bcdbb98ca0e1c7daa40053c
Stern Gerlach <drksugi@gmail.com>
2018-07-22 22:03:49 +0900

    Fix AvailableSizeInfoTable class

--- src/javaiina/AvailableSizeInfoTable.java
+++ src/javaiina/AvailableSizeInfoTable.java
@@ -1,5 +1,5 @@
 
-/* AvailableSizeInfoTableMethod.java*/
+/* AvailableSizeInfoTableMethod.java */
 
 package javaiina;
 
@@ -10,122 +10,93 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.ArrayList;
 
-public class AvailableSizeInfoTableMethod {    
-    private AvailableSizeInfoTableMethod() throws SQLException, ClassNotFoundException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+public class AvailableSizeInfoTable
+{
+    public AvailableSizeInfoTable() throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
-        // test data
-        if(tableExist("AvailableSizeInfo"))
-            stmt.executeUpdate("insert into AvailableSizeInfo values" 
-                + "(0, 0)"
-                + ",(0, 1)"
-                + ",(0, 2)"
-                + ",(1, 0)"
-                + ",(1, 1)"
-                + ",(1, 2)"
-                + ",(2, 0)"
-                + ",(2, 1)"
-                + ",(2, 2)"
-              );
-        stmt.close();
-    }
-    
-    public void reservationInsert(AvailableSize aSize) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("insert into AvailableSizeInfo values("
-            + aSize.getRentalObject().id() + ","
-            + aSize.getSizeInfo().id() + ","
-              + ")"
-         );
+
+        // Add test data
+        if (TableCreateHelper.checkTableExists("AvailableSizeInfo"))
+            stmt.executeUpdate(
+                "insert into AvailableSizeInfo values" +
+                    "(0, 0)" +
+                    ",(0, 1)" + 
+                    ",(0, 2)" +
+                    ",(1, 0)" +
+                    ",(1, 1)" +
+                    ",(1, 2)" +
+                    ",(2, 0)" +
+                    ",(2, 1)" +
+                    ",(2, 2)");
+        
         stmt.close();
     }
 
-    public void reservationDeleteByRentalObject(AvailableSize aSize) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+    public void insert(AvailableSize availableSize) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
-        stmt.executeUpdate("delete from AvailableSizeInfo where rentalObjectId = " + aSize.getRentalObject().id());
-        stmt.close();
-    }
-    
-    public void reservationDeleteBySizeInfo(AvailableSize aSize) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("delete from AvailableSizeInfo where sizeId = " + aSize.getSizeInfo().id());
-        stmt.close();
-    }
-    
-    public void availableSizeInfoUpdate(String column, long updateData, String condition) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update AvailableSizeInfo "
-            + "set " + column + " = " + updateData + " " 
-            + condition
-         );
+            "insert into AvailableSizeInfo values(" +
+                availableSize.getRentalObject().id() + "," +
+                availableSize.getSizeInfo().id() + ")");
         stmt.close();
     }
-    
-    public void availableSizeInfoUpdateSizeId(AvailableSize aSize) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnetion();
+
+    public void deleteWhereRentalObject(RentalObject rentalObject) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update AvailableSizeInfo "
-            + "set sizeId = " + aSize.getSizeInfo().id() + " " 
-            + "where rentalObjectId = " + aSize.getRentalObject().id()
-         ); 
+            "delete from AvailableSizeInfo " +
+                "where rentalObjectId = " + rentalObject.id());
         stmt.close();
     }
-    
-    public void availableSizeInfoUpdateRentalObjectId(AvailableSize aSize) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+
+    public void deleteWhereSizeInfo(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update AvailableSizeInfo "
-            + "set rentalObjectId = " + aSize.getRentalObject().id() + " "  
-            + "where sizeId = " + aSize.getSizeInfo().id()
-         ); 
+            "delete from AvailableSizeInfo " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateAvailableSizeInfo(String columnName, long newValue, String conditionWhere) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update AvailableSizeInfo " +
+                "set " + columnName + " = " + newValue + " " + 
+                "where " + conditionWhere);
         stmt.close();
     }
     
-    public List<AvailableSize> availableSizeSelectAll(AvailableSize aSize) throws SQLException{
+    public List<AvailableSize> availableSizeSelectAll(
+        List<RentalObject> rentalObjects, List<RentalObjectSizeInfo> sizeInfos) throws SQLException
+    {   
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        ResultSet resultSet = stmt.executeQuery("select * from AvailableSizeInfo");
         List<AvailableSize> resultList = new ArrayList<AvailableSize>();
-        RentalObject[] ro;
-        RentalObjectSizeInfo[] rosi;
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet roRs = stmt.executeQuery("select ro.* from RentalObject ro, AvailableSizeInfo a where a.rentalObjectId = " + aSize.getRentalObject().id());
-        ResultSet rosiRs = stmt.executeQuery("select si.* from SizeInfo si, AvailableSizeInfo a where a.sizeId = " + aSize.getSizeInfo().id());
-        ResultSet rs = stmt.executeQuery("select * from AvailableSize where rentalObjectId = " + aSize.getRentalObject().id());
-        for (int i = 0; rosiRs.next(); i++)
-            rosi[i] = new RentalObjectSizeInfo(
-                    rosiRs.getLong("sizeId"),
-                    rosiRs.getString("sizeName"),
-                    rosiRs.getInt("height"),
-                    rosiRs.getInt("weight"),
-                    rosiRs.getInt("waistMin"),
-                    rosiRs.getInt("waistMax"),
-                    rosiRs.getInt("chestWidth"),
-                    rosiRs.getInt("shoulderLength"),
-                    rosiRs.getInt("sleeveLength"),
-                    rosiRs.getInt("inseam")
-                    );    
         
-        for (int i = 0; roRs.next(); i++)
-            ro[i] = new RentalObject(
-                    roRs.getLong("rentalObjectId"),
-                    roRs.getString("rentalObjectName"),
-                    roRs.getString("categoryName"),
-                    rosi,
-                    roRs.getInt("cost")
-                    );
+        for (int i = 0; resultSet.next(); ++i)
+            resultList.add(new AvailableSize(
+                rentalObjects.get((int)resultSet.getLong("rentalObjectId")),
+                sizeInfos.get((int)resultSet.getLong("sizeId"))));
         
-        for (int i = 0; rs.next(); i++) {
-            resultList.add(new AvailableSize(ro[i], rosi[i]));
-        }
-        
-        rs.close();
+        resultSet.close();
         stmt.close();
+        
         return resultList;
     }
 }
