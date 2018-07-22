ae118a117a1af97f790fca9cb5a54c39b3a93ee8
Stern Gerlach <drksugi@gmail.com>
2018-07-22 22:18:09 +0900

    Fix SizeInfoTable class

--- src/javaiina/SizeInfoTable.java
+++ src/javaiina/SizeInfoTable.java
@@ -10,177 +10,197 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.ArrayList;
 
-public class SizeInfoTableMethod {
-    private SizeInfoTableMethod() throws SQLException, ClassNotFoundException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+public class SizeInfoTable
+{
+    public SizeInfoTable() throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
-        // test data
-        if(tableExist("SizeInfo"))
-            stmt.executeUpdate("insert into SizeInfo values" 
-                + "(0, 'A1', 150, 49, 68, 78, 86, 42, 53, 66)"
-                + ",(1, 'A2', 155, 53, 70, 80, 88, 42, 54 69)"
-                + ",(2, 'A3', 160, 57, 72, 83, 92, 43, 56, 71)"
-            );
-        stmt.close();
-    }
-    
-    public void sizeInfoInsert(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("insert into SizeInfo values("
-            + si.id() + ","
-            + "'" + si.sizeName() + "',"
-            + si.height()
-            + si.weight()
-            + si.waistMin()
-            + si.waistMax()
-            + si.chestWidth()
-            + si.shoulderWidth()
-            + si.sleeveLength()
-            + si.inseamLength()
-            + ")"
-        );
+        
+        // Add test data
+        if (TableCreateHelper.checkTableExists("SizeInfo"))
+            stmt.executeUpdate(
+                "insert into SizeInfo values" +
+                    "(0, 'A1', 150, 49, 68, 78, 86, 42, 53, 66)" +
+                    ",(1, 'A2', 155, 53, 70, 80, 88, 42, 54, 69)" +
+                    ",(2, 'A3', 160, 57, 72, 83, 92, 43, 56, 71)");
+        
         stmt.close();
     }
 
-    public void sizeInfoDelete(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+    public void insert(RentalObjectSizeInfo si) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
-        stmt.executeUpdate("delete from SizeInfo where sizeId = " + si.id());
+        
+        stmt.executeUpdate(
+            "insert into SizeInfo values(" +
+                si.id() + "," +
+                "'" + si.sizeName() + "', " +
+                si.height() + ", " +
+                si.weight() + ", " +
+                si.waistMin() + ", " +
+                si.waistMax() + ", " +
+                si.chestWidth() + ", " +
+                si.shoulderWidth() + ", " +
+                si.sleeveLength() + ", " +
+                si.inseamLength() + ")");
         stmt.close();
     }
-    
-    public void sizeInfoUpdate(String column, long updateData, String condition) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+
+    public void delete(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate("delete from SizeInfo where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void update(String columnName, long newValue, String conditionWhere) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set " + columnName + " = " + newValue + " " + 
+                "where " + conditionWhere);
+        stmt.close();
+    }
+
+    public void updateSizeName(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set sizeName = '" + sizeInfo.sizeName() + "' " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateHeight(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set height = " + sizeInfo.height() + " " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateWeight(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set weight = " + sizeInfo.weight() + " " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateWaistMin(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set waistMin = " + sizeInfo.waistMin() + " " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateWaistMax(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate(
+            "update SizeInfo " +
+                "set waistMax = " + sizeInfo.waistMax() + " " +
+                "where sizeId = " + sizeInfo.id());
+        stmt.close();
+    }
+
+    public void updateChestWidth(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
         stmt.executeUpdate(
-            "update SizeInfo "
-            + "set " + column + " = " + updateData + " " 
-            + condition
-        );
+            "update SizeInfo " +
+                "set ChestWidth = " + sizeInfo.chestWidth() + " " +
+                "where sizeId = " + sizeInfo.id());
         stmt.close();
     }
-    
-    public void sizeInfoUpdateSizeName(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+
+    public void updateShoulderWidth(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update SizeInfo "
-            + "set sizeName = '" + si.sizeName() + "' " 
-            + "where sizeId = " + si.id()
-        );
+            "update SizeInfo " +
+                "set shoulderWidth = " + sizeInfo.shoulderWidth() + " " +
+                "where sizeId = " + sizeInfo.id());
         stmt.close();
     }
-    
-    public void sizeInfoUpdateHeight(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnetion();
+
+    public void updateSleeveLength(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update SizeInfo "
-            + "set height = " + si.height() + " " 
-            + "where sizeId = " + si.id()
-        ); 
+            "update SizeInfo " +
+                "set sleeveLength = " + sizeInfo.sleeveLength() + " " +
+                "where sizeId = " + sizeInfo.id());
         stmt.close();
     }
-    
-    public void sizeInfoUpdateWeight(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+
+    public void updateInseamLength(RentalObjectSizeInfo sizeInfo) throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
+        
         stmt.executeUpdate(
-            "update SizeInfo "
-            + "set weight = " + si.weight() + " "  
-            + "where sizeId = " + si.id()
-        ); 
+            "update SizeInfo " +
+                "set inseamLength = " + sizeInfo.inseamLength() + " " +
+                "where sizeId = " + sizeInfo.id());
         stmt.close();
     }
-    
-    public void sizeInfoUpdateWaistMin(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
+
+    public List<RentalObjectSizeInfo> select() throws SQLException
+    {
+        Connection conn = DatabaseAccessManager.getInstance().getConnection();
         Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set waistMin = " + si.waistMin() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public void sizeInfoUpdateWaistMax(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set waistMax = " + si.waistMax() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public void sizeInfoUpdateChestWidth(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set ChestWidth = " + si.chestWidth() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public void sizeInfoUpdateShoulderWidth(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set shoulderWidth = " + si.shoulderWidth() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public void sizeInfoUpdateSleeveLength(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set sleeveLength = " + si.sleeveLength() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public void sizeInfoUpdateInseamLength(RentalObjectSizeInfo si) throws SQLException{
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(
-            "update SizeInfo "
-            + "set inseamLength = " + si.inseamLength() + " "  
-            + "where sizeId = " + si.id()
-        ); 
-        stmt.close();
-    }
-    
-    public List<RentalObjectSizeInfo> sizeInfoSelectAll(RentalObjectSizeInfo si) throws SQLException{
+        
+        ResultSet resultSet = stmt.executeQuery("select * from SizeInfo");
         List<RentalObjectSizeInfo> resultList = new ArrayList<RentalObjectSizeInfo>();
-        Connection conn = DatabaseAccess.getInstance().getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from SizeInfo where sizeId = " + si.id());
-        while(rs.next()) {
+        
+        while (resultSet.next()) {
             resultList.add(new RentalObjectSizeInfo(
-                rs.getLong("sizeId"),
-                rs.getString("sizeName"),
-                rs.getInt("height"),
-                rs.getInt("weight"),
-                rs.getInt("waistMin"),
-                rs.getInt("waistMax"),
-                rs.getInt("chestWidth"),
-                rs.getInt("shoulderLength"),
-                rs.getInt("sleeveLength"),
-                rs.getInt("inseam"))
-            );
+                resultSet.getLong("sizeId"),
+                resultSet.getString("sizeName"),
+                resultSet.getInt("height"),
+                resultSet.getInt("weight"),
+                resultSet.getInt("waistMin"),
+                resultSet.getInt("waistMax"),
+                resultSet.getInt("chestWidth"),
+                resultSet.getInt("shoulderLength"),
+                resultSet.getInt("sleeveLength"),
+                resultSet.getInt("inseam")));
         }
-        rs.close();
+        
+        resultSet.close();
         stmt.close();
+        
         return resultList;
     }
 }
